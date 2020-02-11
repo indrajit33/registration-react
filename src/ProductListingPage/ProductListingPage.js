e5f96d32e850c9ddea64b42891d095f964a7a097
Your Name <indrajit.auddy27@gmail.com>
2020-02-11 20:29:38 +0530

    Added all

---
+++ src/ProductListingPage/ProductListingPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class ProductListingPage extends React.Component {
+    componentDidMount() {
+        this.props.getAllProducts();
+    }
+
+    handleDeleteUser(id) {
+        //return (e) => this.props.deleteUser(id);
+    }
+
+    render() {
+        const { user, users } = this.props;
+        //console.log(users);
+        return (
+            <div> I Am In</div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return { user, users };
+}
+
+const actionCreators = {
+    getAllProducts: userActions.getAllProducts,
+    deleteUser: userActions.delete
+}
+
+const connectedProductListingPage = connect(mapState, actionCreators)(ProductListingPage);
+export { connectedProductListingPage as ProductListingPage };
